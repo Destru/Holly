b8322437bc45151e764e4dbbd8bc3abed9456b0f
Destru Kaneda <alexander@binaerpilot.no>
2017-04-09 01:32:41 -0700

    Version 1.0

--- bot.js
+++ bot.js
@@ -1,28 +1,29 @@
 const Discord = require('discord.js');
-const bot = new Discord.Client({
-  autoReconnect: true
-});
+const NodeSpotifyWebHelper = require('node-spotify-webhelper');
+
+const bot = new Discord.Client({ autoReconnect: true });
+const spotify = new NodeSpotifyWebHelper.SpotifyWebHelper();
 const token = 'MzAwNTM1Njg3NTI1MTcxMjAx.C8t40g.2fx0Ds1_awaYskAqsksJUWk2cNY';
 
 bot.on('ready', () => {
   console.log('Holly is online!');
 });
 
-var nodeSpotifyWebHelper = require('node-spotify-webhelper');
-var spotify = new nodeSpotifyWebHelper.SpotifyWebHelper();
-
 bot.on('message', message => {
-  if (message.content === '!song') {
+  if (message.content === '!playing') {
+    var songData = 'Nothing playing on Spotify right now, dudes.';
+
     spotify.getStatus(function (err, res) {
       if (err) {
         return console.error(err);
       }
 
-      var songData = res.track.artist_resource.name, '-',
-          res.track.track_resource.name);
-    });
+      if (res.track !== 'undefined') {
+        songData = `${res.track.artist_resource.name} — ${res.track.track_resource.name}`;
+      }
 
-    message.channel.sendMessage(songData);
+      message.channel.sendMessage(songData);
+    });
   }
 });
 
