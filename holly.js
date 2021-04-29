7becf0c3f129b7279be3e5c9251e3433b5552802
Destru Kaneda <alexander@binaerpilot.no>
2021-04-29 02:36:00 -0700

    Let's move this too.

--- holly.js
+++ holly.js
@@ -4,8 +4,6 @@ vm.runInThisContext(fs.readFileSync(__dirname + "/source.js"));
 
 const discord = require('discord.js');
 const client = new discord.Client({ autoReconnect: true });
-
-const drinks = [':beer:', ':tropical_drink:', ':cocktail:', ':wine_glass:', ':tumbler_glass:'];
 const games = ['Back to Reality', 'Better Than Life', 'Gunmen of the Apocalypse', 'Play-by-mail Chess'];
 
 client.on('ready', () => {
@@ -18,11 +16,6 @@ client.on('message', message => {
 
   if (command === '!help') {
     message.channel.send(`Please check pinned messages for help, ${message.author}`);
-  }
-
-  else if (command === '!drink') {
-    message.channel.send(`Enjoy, ${message.author}`);
-    message.channel.send(drinks[Math.floor(Math.random() * (0, drinks.length))]);
   }
 });
 
