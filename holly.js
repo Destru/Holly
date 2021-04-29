6eb29993fcc94c97316a3f5f408064402ab39727
Destru Kaneda <alexander@binaerpilot.no>
2021-04-29 02:25:49 -0700

    Repurposing...

--- holly.js
+++ holly.js
@@ -5,10 +5,8 @@ vm.runInThisContext(fs.readFileSync(__dirname + "/source.js"));
 const discord = require('discord.js');
 const client = new discord.Client({ autoReconnect: true });
 
-const binaerpilot = 'ʙɪɴᴀᴇʀᴩɪʟᴏᴛ <:binaerpilot:439812074668032000>';
 const drinks = [':beer:', ':tropical_drink:', ':cocktail:', ':wine_glass:', ':tumbler_glass:'];
 const games = ['Back to Reality', 'Better Than Life', 'Gunmen of the Apocalypse', 'Play-by-mail Chess'];
-const limited_access = 'Please note that access is limited (see `!support` for more information)';
 
 client.on('ready', () => {
   let playingMsg = games[Math.floor(Math.random() * (0, games.length))];
@@ -20,25 +18,6 @@ client.on('message', message => {
 
   if (command === '!help') {
     message.channel.send(`Please check pinned messages for help, ${message.author}`);
-  }
-
-  else if (command === '!torrent' || command === '!binaerpilot') {
-    message.channel.send(`${binaerpilot} Discography torrent is pinned in the <#462734936177115136> channel, ${message.author}`);
-  }
-
-  else if (command === '!patreon' || command === '!support') {
-    message.channel.send(`You can support the ***Cyberpunk Social Club*** on Patreon to unlock special roles, ${message.author}`);
-    message.channel.send('https://www.patreon.com/destru');
-  }
-
-  else if (command === '!backstage') {
-    message.channel.send(`${binaerpilot} Backstage torrent is pinned in the <#831769969095344128> channel, ${message.author}`);
-    message.channel.send(limited_access);
-  }
-
-  else if (command === '!flac') {
-    message.channel.send(`${binaerpilot} FLAC torrent is pinned in the <#831769762518007839> channel, ${message.author}`);
-    message.channel.send(limited_access);
   }
 
   else if (command === '!drink') {
