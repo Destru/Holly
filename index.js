d0a86be3187b0a76aaeb19825e189ed0c476a8e8
Destru Kaneda <alexander@binaerpilot.no>
2021-05-23 14:12:40 -0700

    Let's give people in the bar random compliments. A ping command is useful too.

--- index.js
+++ index.js
@@ -6,34 +6,47 @@ const client = new Discord.Client();
 const cron = require('node-cron');
 const fetch = require('node-fetch');
 
+const complimentChannels = ['836963196916858902'];
+const complimentEmoji = [':heart:', ':heart_eyes:', ':black_heart:', ':blue_heart:', ':brown_heart:', ':green_heart:', ':orange_heart:', ':purple_heart:', ':sparkling_heart:', ':white_heart:', ':yellow_heart:', ':smiling_face_with_3_hearts:', ':kiss:', ':kissing:', ':kissing_heart:', ':kissing_closed_eyes:', ':kissing_smiling_eyes:'];
 const insultUsers = ['400786664861204481'];
+const randomChance = 0.01;
 const status = ['Back to Reality', 'Better Than Life', 'Gunmen of the Apocalypse', 'Play-by-mail Chess'];
 
 cron.schedule('0 */4 * * *', () => {
   client.user.setPresence({
     status: 'online',
     activity: {
-      name: status[(Math.floor(Math.random() * status.length))],
+      name: status[Math.floor(Math.random() * status.length)],
       type: 'PLAYING',
     }
   });
 });
 
 client.on('message', message => {
-  if (insultUsers.includes(message.author.id) && Math.random() < 0.01) {
+  if (insultUsers.includes(message.author.id) && Math.random() < randomChance) {
     fetch('https://insult.mattbas.org/api/insult.json')
       .then(response => response.json())
       .then(data => {
         message.channel.send(`${data.insult}, <@${config.insultUser}>`);
       });
+  }
+
+  if (message.author.bot) return;
+
+  if (complimentChannels.includes(message.channel.id) && Math.random() < randomChance) {
+    fetch('https://complimentr.com/api')
+      .then(response => response.json())
+      .then(data => {
+        message.channel.send(`${data.compliment.charAt(0).toUpperCase() + data.compliment.slice(1)}, ${message.author} ${complimentEmoji[Math.floor(Math.random() * status.length)]}`);
+      });
   } else
-  if (message.startsWith('!ping')) {
+  if (message.content.startsWith('!ping')) {
     message.channel.send(`${Date.now() - message.createdTimestamp}ms / ${Math.round(client.ws.ping)}ms`);
   }
 });
 
 client.on('ready', () => {
-  console.log(`Holly ${process.env.npm_package_version} is online.`);
+  console.log(`Holly ${process.env.npm_package_version || '(nodemon)'} is online.`);
   client.user.setPresence({
     status: 'online',
     activity: {
