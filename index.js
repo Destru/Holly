bda83ca4b70675f939c46af5754a38ac7dc574db
Destru Kaneda <alexander@binaerpilot.no>
2021-05-24 04:27:08 -0700

    Helper function to add channels by name (mostly for legibility). Adds some corporate bullshit. Removes semicolons because this is an eXpErImEnTaL bot.

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
-require('dotenv').config();
-const Discord = require('discord.js');
-const fs = require('fs');
+require('dotenv').config()
+const Discord = require('discord.js')
+const fs = require('fs')
 
-const client = new Discord.Client();
-const cron = require('node-cron');
-const fetch = require('node-fetch');
+const client = new Discord.Client()
+const cron = require('node-cron')
+const fetch = require('node-fetch')
 
-const complimentChannels = ['836963196916858902'];
-const complimentEmoji = [':heart:', ':heart_eyes:', ':black_heart:', ':blue_heart:', ':brown_heart:', ':green_heart:', ':orange_heart:', ':purple_heart:', ':sparkling_heart:', ':white_heart:', ':yellow_heart:', ':smiling_face_with_3_hearts:', ':kiss:', ':kissing:', ':kissing_heart:', ':kissing_closed_eyes:', ':kissing_smiling_eyes:'];
-const insultUsers = ['400786664861204481'];
-const randomChance = 0.01;
-const status = ['Back to Reality', 'Better Than Life', 'Gunmen of the Apocalypse', 'Play-by-mail Chess'];
+const complimentEmojis = [':heart:', ':heart_eyes:', ':black_heart:', ':blue_heart:', ':brown_heart:', ':green_heart:', ':orange_heart:', ':purple_heart:', ':sparkling_heart:', ':white_heart:', ':yellow_heart:', ':smiling_face_with_3_hearts:', ':kiss:', ':kissing:', ':kissing_heart:', ':kissing_closed_eyes:', ':kissing_smiling_eyes:']
+const insultUsers = ['400786664861204481']
+const randomChance = 0.01
+const status = ['Back to Reality', 'Better Than Life', 'Gunmen of the Apocalypse', 'Play-by-mail Chess']
+
+const findChannels = (client, channels) => {
+  return channels.map(name => {
+    return client.channels.cache.find(channel => channel.name.toLowerCase() === name).id
+  })
+}
+let businessChannels = []
+let complimentChannels = []
+
 
 cron.schedule('0 */4 * * *', () => {
   client.user.setPresence({
@@ -19,8 +27,8 @@ cron.schedule('0 */4 * * *', () => {
       name: status[Math.floor(Math.random() * status.length)],
       type: 'PLAYING',
     }
-  });
-});
+  })
+})
 
 client.on('message', message => {
   if (insultUsers.includes(message.author.id) && Math.random() < randomChance) {
@@ -28,10 +36,10 @@ client.on('message', message => {
       .then(response => response.json())
       .then(data => {
         message.channel.send(`${data.insult}, ${message.author}`);
-      });
+      })
   }
 
-  if (message.author.bot) return;
+  if (message.author.bot) return
 
   if (complimentChannels.includes(message.channel.id) && Math.random() < randomChance) {
     fetch('https://complimentr.com/api')
@@ -41,22 +49,37 @@ client.on('message', message => {
         let emoji = complimentEmojis[Math.floor(Math.random() * status.length)];
 
         message.channel.send(`${compliment}, ${message.author} ${emoji}`);
-      });
-  } else
+      })
+  }
+
+  if (businessChannels.includes(message.channel.id) && Math.random() < randomChance) {
+    fetch('https://corporatebs-generator.sameerkumar.website/')
+      .then(response => response.json())
+      .then(data => {
+        let bullshit = data.phrase.charAt(0).toUpperCase() + data.phrase.toLowerCase().slice(1);
+        message.channel.send(`${bullshit} :man_office_worker:`);
+      })
+  }
+
   if (message.content.startsWith('!ping')) {
     message.channel.send(`${Date.now() - message.createdTimestamp}ms / ${Math.round(client.ws.ping)}ms`);
   }
-});
+})
 
 client.on('ready', () => {
-  console.log(`Holly ${process.env.npm_package_version || '(Development)'} is online.`);
+  console.log(`Holly ${process.env.npm_package_version || '(Development)'} is online.`)
+
+  businessChannels = findChannels(client, ['internal-affairs', 'public-relations'])
+  complimentChannels = findChannels(client, ['internal-affairs', 'gay-bar'])
+
   client.user.setPresence({
     status: 'online',
     activity: {
       name: `Destru's OnlyFans`,
       type: 'WATCHING',
     }
-  });
-});
+  })
 
-client.login();
+})
+
+client.login()
