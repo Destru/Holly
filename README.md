1ab02d49bee513a28e2b5ffc5100e2f310a93b2a
Destru Kaneda <alexander@binaerpilot.no>
2021-05-29 02:17:21 -0700

    Let's store the haikus. Adds prettier and removes obsolete helpers. Pink, mwah.

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # Holly
+[![Website](https://img.shields.io/website/https/cyberpunksocial.club.svg?style=flat-square)](https://cyberpunksocial.club/)
+[![Discord](https://img.shields.io/discord/160320676580818951.svg?style=flat-square)](https://discord.gg/f8RAs38C6u)
+[![Code Style: Prettier](https://img.shields.io/badge/code_style-prettier-ff69b4.svg?style=flat-square)](https://github.com/prettier/prettier)
+
 Experimental chatbot for the [Cyberpunk Social Club](https://cyberpunksocial.club).
 
 > Jvgubhg zhfvp, yvsr jbhyq or n zvfgnxr. Naq gubfr jub jrer frra qnapvat jrer gubhtug gb or vafnar ol gubfr jub pbhyq abg urne gur zhfvp. Jr fubhyq pbafvqre rirel qnl ybfg ba juvpu jr unir abg qnaprq ng yrnfg bapr.
--- index.js
+++ index.js
@@ -1,92 +1,148 @@
 require('dotenv').config()
 require('discord-reply')
 const Discord = require('discord.js')
-const fs = require('fs')
-
 const client = new Discord.Client()
-const cron = require('node-cron')
+const db = require('flat-db')
 const fetch = require('node-fetch')
-const syllable = require('syllable')
-const { writeHaiku } = require('./helpers')
+const findahaiku = require('findahaiku')
 
-let csc = 'Cyberpunk Social Club'
-const destru = '160320553322807296';
+const businessChannels = ['845382463685132288', '829717667107700746']
+const complimentChannels = ['845382463685132288', '836963196916858902']
+const complimentEmoji = [
+  ':heart:',
+  ':heart_eyes:',
+  ':black_heart:',
+  ':blue_heart:',
+  ':brown_heart:',
+  ':green_heart:',
+  ':orange_heart:',
+  ':purple_heart:',
+  ':sparkling_heart:',
+  ':white_heart:',
+  ':yellow_heart:',
+  ':smiling_face_with_3_hearts:',
+  ':kiss:',
+  ':kissing:',
+  ':kissing_heart:',
+  ':kissing_closed_eyes:',
+  ':kissing_smiling_eyes:',
+]
 const insultUsers = ['400786664861204481']
 const randomChance = 0.01
-const status = ['Back to Reality', 'Better Than Life', 'Gunmen of the Apocalypse', 'Play-by-mail Chess']
-const testChannel = '845382463685132288'
+const status = [
+  'Back to Reality',
+  'Better Than Life',
+  'Gunmen of the Apocalypse',
+  'Play-by-mail Chess',
+]
 const version = process.env.npm_package_version || '(Development)'
 
-cron.schedule('0 */4 * * *', () => {
-  client.user.setPresence({
-    status: 'online',
-    activity: {
-      name: status[Math.floor(Math.random() * status.length)],
-      type: 'PLAYING',
-    }
-  })
+db.configure({
+  dir: './db',
 })
 
-client.on('message', message => {
+const Haiku = new db.Collection('haikus', {
+  author: '',
+  channel: '',
+  content: '',
+})
+
+client.on('message', (message) => {
+  // bot interactions
   if (insultUsers.includes(message.author.id) && Math.random() < randomChance) {
     fetch('https://insult.mattbas.org/api/insult.json')
-      .then(response => response.json())
-      .then(data => {
-        message.channel.send(`${data.insult}, ${message.author}`);
+      .then((response) => response.json())
+      .then((data) => {
+        message.channel.send(`${data.insult}, ${message.author}`)
+      })
+  } else if (message.author.bot) return
+
+  // haiku
+  const { isHaiku, formattedHaiku } = findahaiku.analyzeText(message.content)
+
+  if (isHaiku) {
+    const embed = new Discord.MessageEmbed()
+      .setColor('#ff00ff')
+      .setDescription(`${formattedHaiku}\n—*${message.author.username}*`)
+
+    Haiku.add({
+      author: message.author.id,
+      channel: message.channel.id,
+      content: formattedHaiku,
+    })
+
+    message.lineReply(embed)
+  } else if (message.content.startsWith('!list-haikus')) {
+    const haikus = Haiku.find()
+      .matches('author', message.author.id + 1)
+      .run()
+    console.log(haikus)
+
+    if (haikus.length > 0) {
+      const embed = new Discord.MessageEmbed().setColor('#ff00ff')
+
+      haikus.forEach((haiku) => {
+        const timestamp = new Date(haiku._ts_).toLocaleString([], {
+          year: 'numeric',
+          month: 'numeric',
+          day: 'numeric',
+        })
+
+        embed.addField(timestamp, haiku.content)
+      })
+
+      message.lineReply(embed)
+    } else message.channel.send(`Oi, you haven't written any haikus yet.`)
+  }
+
+  // cute compliments
+  else if (
+    complimentChannels.includes(message.channel.id) &&
+    Math.random() < randomChance
+  ) {
+    fetch('https://complimentr.com/api')
+      .then((response) => response.json())
+      .then((data) => {
+        let compliment =
+          data.compliment.charAt(0).toUpperCase() + data.compliment.slice(1)
+        let emoji = complimentEmoji[Math.floor(Math.random() * status.length)]
+
+        message.channel.send(`${compliment}, ${message.author} ${emoji}`)
       })
   }
 
-  if (message.author.bot) return
-
-  if (message.content.startsWith('!ping')) {
-    message.channel.send(`${Date.now() - message.createdTimestamp}ms / ${Math.round(client.ws.ping)}ms`);
+  // business talk
+  else if (
+    businessChannels.includes(message.channel.id) &&
+    Math.random() < randomChance
+  ) {
+    fetch('https://corporatebs-generator.sameerkumar.website/')
+      .then((response) => response.json())
+      .then((data) => {
+        let bullshit =
+          data.phrase.charAt(0).toUpperCase() +
+          data.phrase.toLowerCase().slice(1)
+        message.channel.send(`${bullshit} :man_office_worker:`)
+      })
   }
-  else if (message.content.startsWith('!run')) {
-    if (message.author.id === destru) {
-      console.log(`Hi, it's me, you!`)
-    }
-    else message.channel.send(`You're not <@${destru}>, ${message.author}`)
-  }
-  else if(syllable(message.content) === 17) {
-    if (message.content.match(/(https?|ftp|file):\/\//g)) return
-    message.lineReply(`${writeHaiku(message.content).message}—_${message.author.username}_`)
-  }
-  else {
-    const businessChannels = ['845382463685132288', '829717667107700746']
-    const complimentChannels = ['845382463685132288', '836963196916858902']
-    const complimentEmoji = [':heart:', ':heart_eyes:', ':black_heart:', ':blue_heart:', ':brown_heart:', ':green_heart:', ':orange_heart:', ':purple_heart:', ':sparkling_heart:', ':white_heart:', ':yellow_heart:', ':smiling_face_with_3_hearts:', ':kiss:', ':kissing:', ':kissing_heart:', ':kissing_closed_eyes:', ':kissing_smiling_eyes:']
 
-    if (complimentChannels.includes(message.channel.id) && Math.random() < randomChance) {
-      fetch('https://complimentr.com/api')
-        .then(response => response.json())
-        .then(data => {
-          let compliment = data.compliment.charAt(0).toUpperCase() + data.compliment.slice(1);
-          let emoji = complimentEmoji[Math.floor(Math.random() * status.length)];
-
-          message.channel.send(`${compliment}, ${message.author} ${emoji}`);
-        })
-    }
-    else if (businessChannels.includes(message.channel.id) && Math.random() < randomChance) {
-      fetch('https://corporatebs-generator.sameerkumar.website/')
-        .then(response => response.json())
-        .then(data => {
-          let bullshit = data.phrase.charAt(0).toUpperCase() + data.phrase.toLowerCase().slice(1);
-          message.channel.send(`${bullshit} :man_office_worker:`);
-        })
-    }
+  // fuckaneolib
+  else if (message.content.startsWith('!bot-info')) {
+    message.channel.send(
+      `I am Holly, the ship's computer, with an IQ of 6000; the same IQ as 6000 neoliberals.`
+    )
   }
 })
 
 client.on('ready', () => {
   console.log(`Holly ${version} is online.`)
-  csc = client.guilds.cache.get('160320676580818951')
 
   client.user.setPresence({
     status: 'online',
     activity: {
-      name: `Destru's OnlyFans`,
-      type: 'WATCHING',
-    }
+      name: status[Math.floor(Math.random() * status.length)],
+      type: 'PLAYING',
+    },
   })
 })
 
