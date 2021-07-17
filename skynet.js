2a4c9ab65e9ad951489b8d1f052dbe965fd2306b
Destru Kaneda <alexander@binaerpilot.no>
2021-07-17 15:01:44 -0700

    Skynet version.

--- skynet.js
+++ skynet.js
@@ -1,9 +1,16 @@
 require('dotenv').config()
 const Discord = require('discord.js')
 const clones = [process.env.CLONE, process.env.CLONE2, process.env.CLONE3]
+const version = '0.0.2'
 
 clones.forEach((token, i) => {
   const client = new Discord.Client()
+
+  client.on('message', (message) => {
+    if (message.author.id !== '160320553322807296') return
+
+    if (message.content === '!version') message.channel.send(version)
+  })
 
   client.on('ready', () => {
     console.log(`Skynet Clone #${i + 1} is online.`)
