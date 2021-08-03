961fb8931e641f71823f8996ee1e65027647908f
Destru Kaneda <alexander@binaerpilot.no>
2021-08-03 00:56:47 -0700

    Check member is real too... Some kind of connection lag.

--- holly.js
+++ holly.js
@@ -359,7 +359,7 @@ client.on('message', (message) => {
       for (let i = 0; i < 5; i++) {
         let member = csc.members.cache.get(immortalRanked[i].uid)
 
-        if (i === 0) embed.setThumbnail(member.user.avatarURL())
+        if (i === 0 && member) embed.setThumbnail(member.user.avatarURL())
 
         leaderboard.push(
           `\`${i + 1}.\` ${member.user.username} ` +
