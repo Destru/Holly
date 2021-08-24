12dfb7f5bcca3399f29f72d5f71499e57d6ce2a2
Destru Kaneda <alexander@binaerpilot.no>
2021-08-24 11:47:46 -0700

    We still need this.

--- skynet.js
+++ skynet.js
@@ -9,5 +9,14 @@ clones.forEach((token, i) => {
     console.log(`Skynet Clone #${i + 1} online.`)
   })
 
+  client.on('typingStart', (channel) => {
+    if (channel.id === '848997740767346699') {
+      channel.startTyping()
+      setTimeout(() => {
+        channel.stopTyping()
+      }, 10 * 60 * 1000)
+    }
+  })
+
   client.login(token)
 })
